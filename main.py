0a8ad6e23c710e0044c69566c1aeabaeb6047c4c
Marco Mialaret Júnior <89661320+MatmJr@users.noreply.github.com>
2025-09-10 21:49:50 -0300

    fim da aula .

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from src.extract import Extract
+from src.load import Load
 
 
 extract = Extract()
+load = Load()
 
-print(extract.extract_data("Mexico&Argentina"))
+brazil = extract.extract_data("Brazil")
+load.load_data(brazil, "tabela_brazil")
+
+
+
+# Matheus Mergulhão, Lucas Henrique , Aline Karen, Renan Humberto
---
+++ src/load.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+
+class Load():
+
+    def __ini__(self):
+        pass
+
+    def load_data(self, universities, table_name):
+        # Criar o banco e se concectar nele
+        con = sqlite3.connect("universidades.db")
+        c = con.cursor()
+
+        # Criar a tabela no banco
+        c.execute(f'''
+        CREATE TABLE IF NOT EXISTS {table_name}
+                (
+                id INTERGER PRIMARY KEY,
+                name TEXT,
+                country TEXT,
+                state_province TEXT,
+                web_pages TEXT,
+                domains TEXT
+                );
+        ''')
+
+        for university in universities: 
+            c.execute(f'''INSERT INTO {table_name} (name, country, state_province, web_pages, domains) VALUES (?,?,?,?,?);''',
+                    (university.get('name'), 
+                    university.get('country'), 
+                    university.get('state-province'), 
+                    ', '.join(university.get('web_pages', [])), 
+                    ', '.join(university.get('domains', []))))
+
+        con.commit()
+        con.close()
